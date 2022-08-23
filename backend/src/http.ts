72b44785bb47419230d2c4277daa4f97b26b8526
Thinh Tran <tio212@gmail.com>
2022-08-23 22:45:50 +0300

    feat: update 404

--- backend/src/http.ts
+++ backend/src/http.ts
@@ -20,18 +20,14 @@ const server = http.createServer((req, res) => {
     const url = new URL(req.url, `http://${req.headers.host}`);
     const routeName = path.parse(url.pathname).name;
 
-    if (routeName in CONTENT) {
-        // Return HTML
-        const filePath = path.join(__dirname, '..', 'public', CONTENT[routeName] + '.html');
-        fs.readFile(filePath, (err, content) => handleReadFile(err, content, res));
-    } else if (routeName in DATA) {
+    if (routeName in DATA) {
         // Return JSON
         res.writeHead(200, { 'Content-Type': 'application/json' });
         res.end(JSON.stringify(DATA[routeName]));
     } else {
-        // Fallback return 404
-        res.writeHead(404);
-        res.end('<h1>404 - Page not found</h1>');
+        // Return HTML
+        const filePath = path.join(__dirname, '..', 'public', CONTENT[routeName] + '.html');
+        fs.readFile(filePath, (err, content) => handleReadFile(err, content, res));
     }
 });
 
@@ -40,7 +36,17 @@ server.listen(PORT, () => {
 });
 
 const handleReadFile = (err: NodeJS.ErrnoException, content: Buffer, res: http.ServerResponse) => {
-    if (err) throw err;
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(content);
+    if (err) {
+        if (err.code === 'ENOENT') {
+            const errorPath = path.join(__dirname, '..', 'public', '404.html');
+            console.log('errorPath > ', errorPath);
+            fs.readFile(errorPath, (_, content) => {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.end(content);
+            });
+        }
+    } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content);
+    }
 };
